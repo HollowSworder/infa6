c78d3750b13a4d688b3d9eabbf47cbfc308dac81
Баранников Иван ИП-513 <anime.byrynki2019@gmail.com>
2025-10-21 01:44:14 +0700

    Исправлен класс Car

--- Car.java
+++ Car.java
@@ -1,14 +1,13 @@
-public class car {
-
+public class Car {
     private int id;
     private String brand;
     private String model;
     private int year;
     private String color;
-    private int price;
+    private double price;
     private String registrationNumber;
 
-    public function __construct() {
+    public Car(int id, String brand, String model, int year, String color, double price, String registrationNumber) {
         this.id = id;
         this.brand = brand;
         this.model = model;
@@ -17,46 +16,33 @@ public class car {
         this.price = price;
         this.registrationNumber = registrationNumber;
     }
-    public int getId() { 
-        return id;
+
+    // Геттеры
+    public String getBrand() {
+        return brand;
     }
-    public void setId(int id) { 
-        this.id = id; 
+
+    public String getModel() {
+        return model;
     }
-    public String getBrand() { 
-        return brand; 
+
+    public int getYear() {
+        return year;
     }
-    public void setBrand(String brand) { 
-        this.brand = brand;
-    }
-    public String getModel() { 
-        return model; 
-    }
-    public void setModel(String model) { 
-        this.model = model;
-    }
-    public int getYear() { 
-        return year; 
-    }
-    public void setYear(int year) { 
-        this.year = year; 
-    }
+
     public String getColor() {
-         return color; 
+        return color;
     }
-    public void setColor(String color) { 
-        this.color = color; 
+
+    public double getPrice() {
+        return price;
     }
-    public double getPrice() { 
-        return price; 
-    }
-    public void setPrice(double price) { 
-        this.price = price; 
-    }
-    public String getRegistrationNumber() { 
+
+    public String getRegistrationNumber() {
         return registrationNumber;
     }
-    public void setRegistrationNumber(String registrationNumber) { 
-        this.registrationNumber = registrationNumber; 
+
+    public int getId() {
+        return id;
     }
 }
