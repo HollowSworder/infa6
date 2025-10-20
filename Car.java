2519e7e3d63bff6206921943a577225024c8a283
Баранников Иван ИП-513 <anime.byrynki2019@gmail.com>
2025-10-21 01:55:01 +0700

    Добавлен класс Car

--- Car.java
+++ Car.java
@@ -17,32 +17,51 @@ public class Car {
         this.registrationNumber = registrationNumber;
     }
 
-    // Геттеры
-    public String getBrand() {
-        return brand;
+    public int getId() { 
+        return id;
+     }
+    public void setId(int id) {
+         this.id = id;
+         }
+    public String getBrand() { 
+        return brand; 
+    }
+    public void setBrand(String brand) { 
+        this.brand = brand; 
     }
 
-    public String getModel() {
-        return model;
+    public String getModel() { 
+        return model; 
     }
+    public void setModel(String model) { 
+        this.model = model;
+     }
 
-    public int getYear() {
-        return year;
+    public int getYear() { 
+        return year; 
+    }
+    public void setYear(int year) { 
+        this.year = year;
     }
 
     public String getColor() {
-        return color;
+         return color; 
+        }
+    public void setColor(String color) { 
+        this.color = color; 
     }
 
-    public double getPrice() {
-        return price;
+    public double getPrice() { 
+        return price; 
     }
+    public void setPrice(double price) {
+         this.price = price;
+         }
 
-    public String getRegistrationNumber() {
+    public String getRegistrationNumber() { 
         return registrationNumber;
-    }
-
-    public int getId() {
-        return id;
+     }
+    public void setRegistrationNumber(String registrationNumber) {
+         this.registrationNumber = registrationNumber; 
     }
 }
